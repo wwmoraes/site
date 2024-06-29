6adf6a4353b755cf7798b631e7bacac6e9dd70df
William Artero <git@artero.dev>
2024-06-29 12:28:00 +0200

    chore: added go's commitlint to nix shell
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -1,9 +1,32 @@
 {
   # pkgs ? import <nixpkgs> { }
   pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-23.11-darwin.tar.gz") {}
-}: with pkgs; mkShell {
-  packages = with pkgs; [
+}: let
+  commitlint = pkgs.buildGoModule rec {
+    pname = "commitlint";
+    version = "0.10.1";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "conventionalcommit";
+      repo = "commitlint";
+      rev = "v${version}";
+      hash = "sha256-OJCK6GEfs/pcorIcKjylBhdMt+lAzsBgBVUmdLfcJR0=";
+    };
+
+    # vendorHash = pkgs.lib.fakeHash;
+    vendorHash = "sha256-4fV75e1Wqxsib0g31+scwM4DYuOOrHpRgavCOGurjT8=";
+
+    meta = with pkgs.lib; {
+      description = "commitlint checks if your commit messages meets the conventional commit format";
+      homepage = "https://github.com/conventionalcommit/commitlint";
+      license = licenses.mit;
+      maintainers = with maintainers; [ wwmoraes ];
+    };
+  };
+in with pkgs; mkShell {
+  packages = [
     checkmake
+    commitlint
     editorconfig-checker
     fish
     git
