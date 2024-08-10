002718e9d2f0dc41a5ccef5bcaf04009a866b659
William Artero <git@artero.dev>
2024-08-10 18:39:06 +0200

    chore: isolate nix package contexts
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -1,16 +1,14 @@
 let
-  nixpkgs = fetchTarball {
+  pkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
     sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  };
-  pkgs = import nixpkgs {};
-  nixpkgs-unstable = fetchTarball {
+  }) {};
+  unstable = import (fetchTarball {
     name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
     # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
     url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
     sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
-  };
-  unstable = import nixpkgs-unstable {};
+  }) {};
   kaizen = import (fetchTarball {
     name = "kaizen-8075b45edf93d8f95a00958fd3a1cc606ba3405c";
     url = "https://github.com/wwmoraes/kaizen/archive/8075b45edf93d8f95a00958fd3a1cc606ba3405c.tar.gz";
@@ -18,27 +16,28 @@ let
   }) { inherit pkgs; };
   inherit (pkgs) lib mkShell;
 in mkShell rec {
-  packages = with pkgs; [
-    editorconfig-checker
-    git
-    go-task
-    gofumpt
-    hugo
-    imagemagick
+  packages = [
     kaizen.go-commitlint
-    lefthook
-    markdownlint-cli
-    stylelint # TODO replace with native code tool
-    typos
+    pkgs.editorconfig-checker
+    pkgs.git
+    pkgs.go-task
+    pkgs.gofumpt
+    pkgs.hugo
+    pkgs.imagemagick
+    pkgs.lefthook
+    pkgs.markdownlint-cli
+    pkgs.stylelint # TODO replace with native code tool
+    pkgs.typos
+    pkgs.vale
+    pkgs.yarn # TODO remove after replacing stylelint
     unstable.go
     unstable.golangci-lint
-    vale
-    yarn # TODO remove after replacing stylelint
   ] ++ lib.optionals (builtins.getEnv "CI" != "") [ # CI-only
   ] ++ lib.optionals (builtins.getEnv "CI" == "") [ # local-only
-    gopls
-    gotools
     kaizen.gopium
+    kaizen.goutline
+    pkgs.gopls
+    pkgs.gotools
     ## TODO kroki
   ];
 
