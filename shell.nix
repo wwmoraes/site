7525983c10915264a5672a0d6efcaac723a0102f
William Artero <git@artero.dev>
2024-11-06 23:59:40 +0100

    chore: updated shell.nix
    Organised dependencies to keep CI tidy
    
    Signed-off-by: William Artero <git@artero.dev>

--- shell.nix
+++ shell.nix
@@ -1,50 +1,54 @@
+{ system ? builtins.currentSystem
+}:
 let
   pkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
     sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
-  }) {};
-  unstable = import (fetchTarball {
-    name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
-    # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
-    url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
-    sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
-  }) {};
-  kaizen = import (fetchTarball {
-    name = "kaizen-d6cde304893d8c1f55789d28c35f26b6256d8f37";
-    url = "https://github.com/wwmoraes/kaizen/archive/d6cde304893d8c1f55789d28c35f26b6256d8f37.tar.gz";
-    sha256 = "1whyxwaw70b5pqj4q2k12fk51c1cs00r7vlqc9k059ddq7zmb6yy";
-  }) { inherit pkgs; };
+  }) {
+    inherit system;
+    config.packageOverrides = pkgs: {
+      nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+      };
+      unstable = import (fetchTarball {
+        name = "nixos-unstable-a14c5d651cee9ed70f9cd9e83f323f1e531002db";
+        # url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-unstable.tar.gz";
+        url = "https://github.com/NixOS/nixpkgs/archive/a14c5d651cee9ed70f9cd9e83f323f1e531002db.tar.gz";
+        sha256 = "1b2dwbqm5vdr7rmxbj5ngrxm7sj5r725rqy60vnlirbbwks6aahb";
+      }) { inherit system pkgs; };
+    };
+  };
   inherit (pkgs) lib mkShell;
 in mkShell rec {
   packages = [
-    kaizen.go-commitlint
     pkgs.editorconfig-checker
     pkgs.go-task
     pkgs.gofumpt
     pkgs.hugo
-    pkgs.imagemagick
     pkgs.lefthook
     pkgs.markdownlint-cli
     pkgs.stylelint # TODO replace with native code tool
     pkgs.typos
+    pkgs.unstable.go
+    pkgs.unstable.golangci-lint
     pkgs.vale
     pkgs.yarn # TODO remove after replacing stylelint
-    unstable.go
-    unstable.golangci-lint
   ] ++ lib.optionals (builtins.getEnv "CI" != "") [ # CI-only
   ] ++ lib.optionals (builtins.getEnv "CI" == "") [ # local-only
-    kaizen.gopium
-    kaizen.goutline
-    kaizen.kroki-cli
+    pkgs.dockerfile-language-server-nodejs
     pkgs.git
     pkgs.gopls
     pkgs.gotools
-    unstable.delve
-    unstable.gopls
-    unstable.golangci-lint-langserver
-    unstable.gotools
+    pkgs.imagemagick
     pkgs.markdown-oxide
-    pkgs.dockerfile-language-server-nodejs
+    pkgs.nur.repos.wwmoraes.go-commitlint
+    pkgs.nur.repos.wwmoraes.gopium
+    pkgs.nur.repos.wwmoraes.goutline
+    pkgs.nur.repos.wwmoraes.kroki-cli
+    pkgs.unstable.delve
+    pkgs.unstable.golangci-lint-langserver
+    pkgs.unstable.gopls
+    pkgs.unstable.gotools
     ## TODO kroki
   ];
 
