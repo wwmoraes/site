191f2119eff561a78db809bb51ad6bc1035d749d
William Artero <git@artero.dev>
2026-05-24 00:21:16 +0200

    ci: fix commit check
    ps-id: 99f03bc9-31f3-4863-8e07-003f8125140d
    Signed-off-by: William Artero <git@artero.dev>

--- .meta/make/git.mk
+++ .meta/make/git.mk
@@ -1,7 +1,5 @@
-check::
-	$(info checking if commit messages follow conventions...)
-	@cog check --from-latest-tag > /dev/null
-
 .PHONY: hooks/*
 hooks/pre-commit: check
 hooks/pre-push: all test check/git
+	$(info checking if commit messages follow conventions...)
+	cog check > /dev/null
